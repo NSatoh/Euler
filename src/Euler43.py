732c3f63c3f6f124c8661a4068e1505afb478794
N. Satoh <nsatoh_3_1415@hotmail.co.jp>
2012-08-12 22:23:24 +0900

    complete 43 (so-so ver.)

--- src/Euler43.py
+++ src/Euler43.py
@@ -15,11 +15,11 @@ Created on 2012/08/11
 #
 # 使わなそうな条件も多いが、とりあえずこんなところか。
 # 17の倍数でまずd8 d9 d10 を絞るのが速そう？
+# 数字を数で扱うと面倒そう。桁で区切ってリストに突っ込んだ方がいいかも
 
-def hasDistinctDigits(num):
-    s = str(num)
-    for i in xrange(len(s)):
-        if s[i] in s[i+1:]:
+def hasDistinctDigits(list):
+    for i in xrange(len(list)):
+        if list[i] in list[i+1:]:
             return False
     return True
 
@@ -27,25 +27,49 @@ def hasDistinctDigits(num):
 #print hasDistinctDigits(123345)
 
 admissible = []
-for i in xrange(2,1000/7):
-    if hasDistinctDigits(7*i):
-        admissible += [7*i]
 
-admissible.remove(70) # 1つだけ明らかに例外なので除外。あとは大丈夫
-            
+for i in xrange(2,100/17+1):
+    s = str(17*i)
+    l = [0,int(s[0]),int(s[1])]
+    if hasDistinctDigits(l):
+        admissible += [l]
+
+for i in xrange(100/17+1,1000/17):
+    s = str(17*i)
+    l = [int(s[0]),int(s[1]),int(s[2])]
+    if hasDistinctDigits(l):
+        admissible += [l]
+          
 #print admissible
-#print len(admissible)
+#print len(admissible) # 候補少ないな。これくらいなら、約数毎に判定法とか使わないで、まとめて処理していいか。
 
-def nextDigit(admissible_num): # すべての桁が異なる、という条件で、次の桁に使える数のリストを返す
-    s = str(admissible_num)
+def nextDigit(list): # すべての桁が異なる、という条件で、次の桁に使える数のリストを返す
     next = range(10)
-    for i in s:
-        next.remove(int(i))
+    for i in list:
+        next.remove(i)
     return next
 
+divisors = [13,11,7,5,3,2]
 
+for d in divisors:
+    new_admissible = []
+    for l in admissible:
+        next = nextDigit(l)
+        for digit in next:
+            if (100*digit + 10*l[0] + l[1]) % d == 0:
+                new_admissible += [[digit]+l]
+    print new_admissible
+    print len(new_admissible)
+    admissible = list(new_admissible)
+    
+sumDigit = [0]*10
+for l in admissible:
+    pandigital = nextDigit(l) + l # あと1桁残っているはずなので
+    sumDigit = map((lambda x,y: x+y),sumDigit,pandigital)
+    print sumDigit
 
+sum = 0    
+for i in xrange(10):
+    sum += 10**(9-i) * sumDigit[i]
 
-
-
-
+print sum
